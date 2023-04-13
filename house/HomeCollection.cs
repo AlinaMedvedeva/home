3270b299f7ced48c04259f75fbc3ec915b1770fa
AlinaMedvedeva <116735202+AlinaMedvedeva@users.noreply.github.com>
2023-04-13 15:44:13 +0700

    Add files via upload

---
+++ house/HomeCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace house
+{
+    class HomeCollection
+    {
+        List<Home> lst = new List<Home>();
+        public HomeCollection()
+        {
+            FileStream fin = new FileStream("G:\\home.txt", FileMode.Open);
+            StreamReader rd = new StreamReader(fin, Encoding.UTF8);
+            string line;
+            string [] split;
+            while((line = rd.ReadLine()) != null)
+            {
+                split = line.Split("+");
+                Home h = new Home(Int32.Parse(split[0]), Int32.Parse(split[1]),
+                    split[2], Int32.Parse(split[3]), Int32.Parse(split[4]), Int32.Parse(split[5]));
+                lst.Add(h);
+            }
+            fin.Close();
+        }
+        public void print(int tag, ListView lb)
+        {
+            ListViewItem item;
+            foreach(Home h in lst)
+            {
+                if (h.connect == tag)
+                {
+                    item = new ListViewItem(h.address);
+                    item.ImageIndex = 2;
+                    item.Tag = h.id;
+                    lb.Items.Add(item);
+                }
+            }
+        }
+
+        public void change(ListViewItem lw, TreeNode targetNode)
+        {
+            String s = lw.Text;
+            foreach(Home h in lst)
+            {
+                if(h.address.Equals(s))
+                {
+                    h.connect = Convert.ToInt32(targetNode.Tag);
+                }
+            }
+        }
+    }
+}
